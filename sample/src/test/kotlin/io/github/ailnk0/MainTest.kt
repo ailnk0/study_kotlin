84c2ee2c16876cafb2b8dad77fe142e09d3b82db
Benedict <ailnk@naver.com>
2023-10-12 14:59:34 +0900

    add test for Function

--- sample/src/test/kotlin/io/github/ailnk0/MainTest.kt
+++ sample/src/test/kotlin/io/github/ailnk0/MainTest.kt
@@ -1,11 +1,63 @@
 package io.github.ailnk0
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class MainTest {
     @Test
-    fun test() {
-        assertEquals(1, 1)
+    fun testInfixFun() {
+        run {
+            infix fun Int.times(str: String) = str.repeat(this)
+            val expected = "Bye-Bye-"
+            val actual = 2 times "Bye-"
+            Assertions.assertEquals(expected, actual)
+        }
+
+        // Eg. Infix is already defined in Pair
+        run {
+            val expected = Pair("name", "Lee")
+            val actual = "name" to "Lee"
+            Assertions.assertEquals(expected, actual)
+        }
+
+        run {
+            class Person(val name: String) {
+                val likes = mutableListOf<Person>()
+                infix fun like(that: Person) = this.likes.add(that)
+            }
+
+            val kim = Person("Kim")
+            val lee = Person("Lee")
+            val ok = kim like lee
+
+            Assertions.assertTrue(ok)
+            Assertions.assertTrue(kim.likes.contains(lee))
+        }
+    }
+
+    @Test
+    fun testOpFun() {
+        run {
+            operator fun Int.times(str: String) = str.repeat(this)
+            val expected = "Bye-Bye-"
+            val actual = 2 * "Bye-"
+            Assertions.assertEquals(expected, actual)
+        }
+
+        run {
+            val text = "Bye-Bye-"
+            operator fun String.get(range: IntRange) = substring(range)
+            val expected = "e-B"
+            val actual = text[2..4]
+            Assertions.assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun testVararg() {
+        fun getLog(vararg log: String, prefix: String) = log.joinToString(prefix = prefix)
+        val expected = "[LOG] Alpha, Beta, Gamma"
+        val actual = getLog("Alpha", "Beta", "Gamma", prefix = "[LOG] ")
+        Assertions.assertEquals(expected, actual)
     }
 }
