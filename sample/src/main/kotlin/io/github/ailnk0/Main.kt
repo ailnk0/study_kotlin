d73bc4b8009cc11e994b322a973d1adab4c9b55c
Benedict <ailnk@naver.com>
2023-10-12 11:32:25 +0900

    add default unit-test and change package

--- sample/src/main/kotlin/io/github/ailnk0/Main.kt
+++ sample/src/main/kotlin/io/github/ailnk0/Main.kt
@@ -1,3 +1,5 @@
+package io.github.ailnk0
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
---
+++ sample/src/test/kotlin/io/github/ailnk0/MainTest.kt
@@ -0,0 +1,11 @@
+package io.github.ailnk0
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun test() {
+        assertEquals(1, 1)
+    }
+}
